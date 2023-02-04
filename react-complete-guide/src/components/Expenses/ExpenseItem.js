c90524acdf21ea749a1f1ac40cd9bcae9fcbfa12
Srinu2231 <121787279+Srinu2231@users.noreply.github.com>
2023-02-04 15:43:20 +0530

    Upload files

---
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
+import Card from "../UI/Card";
+
+function ExpenseItem(props) {
+  // useSate is a React Hook.
+  // It will act as a state whenever any change happen in the component it will re-evaluate it.
+//   const [title, setTitle] = useState(props.title);
+//   function clickHandler() {
+//     setTitle("Updated!!!");
+//   }
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+      {/* <button onClick={clickHandler}>Change Title</button> */}
+    </Card>
+  );
+}
+
+export default ExpenseItem;
